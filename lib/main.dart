ec10f16f7e89f82bdfdf70b7a836ff16a633a297
tanvi1706 <tanvikrishnapatil@tanvis-MBP.lan>
2021-03-11 07:13:00 -0600

    first flutter app

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Image(
+            image: AssetImage('Images/diamond.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
